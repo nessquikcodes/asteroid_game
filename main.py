06b91fdac4ac1622a99bb17c7f517bbaa58f2295
Vanessa Marron <vanessa.m.marron@gmail.com>
2025-05-18 22:08:59 -0700

    Added containers for Player object
    Signed-off-by: Vanessa Marron <vanessa.m.marron@gmail.com>

--- main.py
+++ main.py
@@ -6,18 +6,28 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    updatable = pygame.sprite.Group() # all objects that need to be updated
+    drawable = pygame.sprite.Group() # all objects that need to be drawn
         
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
-            
+        updatable.update(dt)
+        
         screen.fill("black")
-        player.draw(screen)
+        
+        for object in drawable:
+            object.draw(screen)
+            
         pygame.display.flip()
     
         # limit the framerate to 60 FPS
