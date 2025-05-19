7877ac4b35ffcf5d0f9fb42f5ea2c2dbbb5788d4
Vanessa Marron <vanessa.m.marron@gmail.com>
2025-05-18 21:53:26 -0700

    created update and rotate method to player
    Signed-off-by: Vanessa Marron <vanessa.m.marron@gmail.com>

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.update(dt)
             
         screen.fill("black")
         player.draw(screen)
