40a164fd7b9f0aab1b0a269590d75a2b77f2bea5
Vanessa Marron <vanessa.m.marron@gmail.com>
2025-05-18 21:12:06 -0700

    Added clock and limited the framerate to 60 FPS
    Signed-off-by: Vanessa Marron <vanessa.m.marron@gmail.com>

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    print("Starting Asteroids!")
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
-    
-    while pygame.get_init():
+    clock = pygame.time.Clock()
+    dt = 0
+        
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-        screen.fill((0, 0, 0))
+                return
+            
+        screen.fill("black")
         pygame.display.flip()
+    
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == '__main__':
     main()
